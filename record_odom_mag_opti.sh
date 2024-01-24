f0abd09189a35bc0f0d8736dd6fb7152d84d2356
0Unkn0wn <37101806+0Unkn0wn@users.noreply.github.com>
2024-01-24 10:19:56 +0100

    Create record_odom_mag_opti.sh
    uploaded from local to remote

---
+++ record_odom_mag_opti.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+. ~/magslam_ws/install/setup.bash
+ros2 launch bluespace_ai_xsens_mti_driver xsens_mti_node.launch.py &
+ros2 launch mocap_optitrack mocap.launch.py &
+ros2 bag record /odom /imu/mag /imu/dq /imu/dv /imu/time_ref /Robot_1/ground_pose /Robot_1/pose /tf
